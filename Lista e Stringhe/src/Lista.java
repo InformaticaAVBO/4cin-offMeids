1ba8767addf94d422e56301a1d0e407260a5fac1
offMeids <manuel.madeo@aldini.istruzioneer.it>
2025-12-16 07:45:45 +0000

    ghjfgh

--- Lista e Stringhe/src/Lista.java
+++ Lista e Stringhe/src/Lista.java
@@ -1,4 +1,34 @@
-public void remove(String s) {
+public class Lista {
+    Nodo root;
+
+    public Lista() {
+        root = null;
+    }
+
+    public void addTail(Nodo n) {
+        if (root == null) {
+            root = n;
+        } else {
+            Nodo temp = root;
+            while (temp.getNext() != null) {
+                temp = temp.getNext();
+            }
+            temp.setNext(n);
+        }
+    }
+
+    public void addHead(Nodo n) {
+        Nodo temp = root;
+        root = n;
+        n.setNext(temp);
+    }
+
+    public void add(String s) {
+        Nodo n = new Nodo(s);
+        addTail(n);
+    }
+
+    public void remove(String s) {
         if (root == null) return;
 
         if (!exists(s)) return;
@@ -32,3 +62,14 @@ public void remove(String s) {
         }
         return false;
     }
+
+    public String toString() {
+        String s = "La lista contiene: ";
+        Nodo temp = root;
+        while (temp != null) {
+            s += temp.getValue() + " - ";
+            temp = temp.getNext();
+        }
+        return s + "\n";
+    }
+}
